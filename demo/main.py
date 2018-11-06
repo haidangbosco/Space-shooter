6238f07e1fd083ac570126d04d90a75cc6024747
levanduc094 <levanduc094@gmail.com>
2018-11-07 00:37:36 +0700

    thêm vật phẩm

--- demo/main.py
+++ demo/main.py
@@ -3,6 +3,7 @@ import pdb
 from copy import *
 import numpy
 from pygame import *
+from os import path
 
 red=(255,0,0)
 green=(0,255,0)
@@ -13,11 +14,25 @@ black=(0,0,0)
 dw=600
 dh=400
 fps=60
+BAR_LENGTH = 100
+BAR_HEIGHT = 10
 screen=pygame.display.set_mode([dw,dh])
 pygame.display.set_caption("Space Shooter")
 clock=pygame.time.Clock()
+#-------------------assets folder---------------------------
+img_dir = path.join(path.dirname(__file__), 'assets')
+sound_folder = path.join(path.dirname(__file__), 'sounds')
 #-------------------sounds-----------------------------------
 #-------------------images-----------------------------------
+## load power ups
+powerup_images = {}
+powerup_images['shield'] = pygame.image.load(path.join(img_dir, 'shield_gold.png')).convert()
+powerup_images['gun'] = pygame.image.load(path.join(img_dir, 'bolt_gold.png')).convert()
+# player mini
+player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png')).convert()
+player_mini_img = pygame.transform.scale(player_img, (25, 19))
+player_mini_img.set_colorkey(black)
+# explosion_img
 explosion_sheet =  pygame.image.load('explosion.png')
 explosion_img = []
 for i in range(0,3):
@@ -89,6 +104,9 @@ class NightRider(pygame.sprite.Sprite):
         self.onscreen_y = copy(self.y)
         self.kill = False
         self.explode = False
+        self.shield = 100
+        self.lives = 3
+        self.power = 1
         self.imgs = [night_raider_img] + explosion_img
         self.index_img = 0
         self.upgrade_level = 1 # different level different bullet type
@@ -128,6 +146,88 @@ class NightRider(pygame.sprite.Sprite):
                 self.GetRect()
                 self.kill = True if self.index_img == len(self.imgs) - 1 else False
 
+# defines the sprite for Powerups
+class Pow(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = random.choice(['shield', 'gun'])
+        self.image = powerup_images[self.type]
+        self.image.set_colorkey(black)
+        self.rect = self.image.get_rect()
+        ## place the bullet according to the current position of the player
+        self.rect.center = center
+        self.speedx = 2
+
+    def update(self):
+        """should spawn right in front of the player"""
+        self.rect.x -= self.speedx
+        ## kill the sprite after it moves over the top border
+        if self.rect.top > dh:
+            self.kill()
+
+def main_menu():
+    global screen
+
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.ogg"))
+    pygame.mixer.music.play(-1)
+
+    title = pygame.image.load(path.join(img_dir, "main.png")).convert()
+    title = pygame.transform.scale(title, (dw, dh), screen)
+
+    screen.blit(title, (0,0))
+    pygame.display.update()
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q:
+                pygame.quit()
+                quit()
+        elif ev.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        else:
+            draw_text(screen, "Press [ENTER] To Begin", 30, dw/2, dh/2)
+            draw_text(screen, "or [Q] To Quit", 30, dw/2, (dh/2)+40)
+            pygame.display.update()
+
+    #pygame.mixer.music.stop()
+    ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
+    ready.play()
+    screen.fill(black)
+    draw_text(screen, "GET READY!", 40, dw/2, dh/2)
+    pygame.display.update()
+
+def draw_text(surf, text, size, x, y):
+    ## selecting a cross platform font to display the score
+    font = pygame.font.Font(pygame.font.match_font('arial'), size)
+    text_surface = font.render(text, True, white)       ## True denotes the font to be anti-aliased
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+def draw_lives(surf, x, y, lives, img):
+    for i in range(lives):
+        img_rect= img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
+
+def draw_shield_bar(surf, x, y, pct):
+    # if pct < 0:
+    #     pct = 0
+    pct = max(pct, 0)
+    ## moving them to top
+    # BAR_LENGTH = 100
+    # BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pygame.Rect(x, y, fill, BAR_HEIGHT)
+    pygame.draw.rect(surf, green, fill_rect)
+    pygame.draw.rect(surf, white, outline_rect, 2)
+
 class MainGame(object):
     def __init__(self):
         screen.fill(white)
@@ -138,13 +238,26 @@ class MainGame(object):
         self.ride = NightRider()
         self.kill_beam = KillBeam()
         self.bullet_gr = pygame.sprite.Group()
+        self.powerup = pygame.sprite.Group()
+        self.score = 0
         self.camera = self.ride
         self.camera_offset = 60
         self.keys = {"up_key": False,
         "down_key": False,
         "right_key": False,
         "a_key":False}
-        while (True):
+        running = True
+        menu_display = True
+        while (running):
+            if menu_display:
+                main_menu()
+                pygame.time.wait(3000)
+                # Stop menu music
+                pygame.mixer.music.stop()
+                # Play the gameplay music
+                pygame.mixer.music.load(path.join(sound_folder, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+                pygame.mixer.music.play(-1)  ## makes the gameplay sound in an endless loop
+                menu_display = False
             # pdb.set_trace()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -173,6 +286,19 @@ class MainGame(object):
             self.Update()
             pygame.display.update()
             clock.tick(fps)
+            # if the player hit a power up
+            hits = pygame.sprite.spritecollide(self.ride, self.powerup, True)
+            for hit in hits:
+                if hit.type == 'shield':
+                    self.ride.shield += random.randrange(10, 30)
+                    if self.ride.shield >= 100:
+                        self.ride.shield = 100
+                if hit.type == 'gun':
+                    if self.ride.upgrade_level == 1:
+                        self.ride.upgrade_level = 0
+                    else:
+                        self.ride.upgrade_level = 1
+
 
     #demo
     def Update(self):
@@ -191,6 +317,8 @@ class MainGame(object):
                 move_from_kill_beam = -2
             if self.keys["a_key"]:
                 self.ride.ShootBullet(self.bullet_gr)
+                self.ride.ShootBullet(self.bullet_gr)
+                self.powerup.add(Pow((600, random.randrange(1,200))))
         else:
             self.ride.MoveFordward(-2)
             move_from_kill_beam = -2
@@ -202,6 +330,15 @@ class MainGame(object):
         # update bullets
         # pdb.set_trace()
         self.bullet_gr.update()
+        # draw powerup
+        self.powerup.update()
+        self.powerup.draw(screen)
+        # draw score
+        draw_text(screen, str(self.score), 18, dw / 2, 10)  ## 10px down from the screen
+        # draw shield bar
+        draw_shield_bar(screen, 5, 5, self.ride.shield)
+        # draw lives
+        draw_lives(screen, dw - 100, 5, self.ride.lives, player_mini_img)
         # print(self.bullet_gr)
         # # check for impact
         temp_list = [self.ride]
